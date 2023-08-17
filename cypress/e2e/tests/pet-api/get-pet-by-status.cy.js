f7bbe69e50302ebc116844909614bc1d4b87ab1b
unknown <limavibrandao@gmail.com>
2023-08-17 19:01:37 -0300

    GET pet by status 'pending' successfully created

--- cypress/e2e/tests/pet-api/get-pet-by-status.cy.js
+++ cypress/e2e/tests/pet-api/get-pet-by-status.cy.js
@@ -1,9 +1,16 @@
 /// <reference types='Cypress' />
 
 describe('GET /pet/findByStatus API', () => {
-    it('should return 200 when a valid status is entered', () => {
+    it("should return 200 when the status 'available' is entered", () => {
         cy.request('GET', 'https://petstore.swagger.io/v2/pet/findByStatus?status=available').then(response => {
             expect(response.status).to.eq(200)
         })
     })
+    it("should return 200 when the status 'pending' is entered", () => {
+        cy.request('GET', 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending').then(response => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
 })
+
