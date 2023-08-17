77d8a0e60dcbeda3b82c520e0680287ee5f65634
unknown <limavibrandao@gmail.com>
2023-08-17 17:23:03 -0300

    DELETE order by id finished

--- cypress/e2e/tests/store-api/delete-order.cy.js
+++ cypress/e2e/tests/store-api/delete-order.cy.js
@@ -1,6 +1,17 @@
 /// <reference types='Cypress' />
 
+import ORDER from '../../../support/objects/order-object.js';
+
 describe('DELETE /store/order/{orderId} API', () => {
+    beforeEach(() => {
+        cy.request(
+            'POST',
+            'https://petstore.swagger.io/v2/store/order',
+            ORDER)
+          .then(response => {
+            expect(response.status).to.eq(200)
+          })
+    })
 
     it('should return 200 when the order is successfully deleted', () => {
         cy.request('DELETE', `https://petstore.swagger.io/v2/store/order/5`).then((response) => {
