4babb11017aa6f6b37126baef1e28fc6a590afaf
unknown <limavibrandao@gmail.com>
2023-08-18 18:37:34 -0300

    PUT user/username sucessfully created

---
+++ cypress/e2e/tests/user-api/update-user.cy.js
@@ -0,0 +1,20 @@
+////// <reference types='Cypress' />
+
+describe('PUT /user/username API', () => {
+    it('should return 200 when the user is updated', () => {
+        cy.request('PUT', 'https://petstore.swagger.io/v2/user/bob',
+         {
+            id: 1,
+            username: "bob",
+            firstName: "bob",
+            lastName: "mcdonald",
+            email: "bobmcdonald@email.com",
+            password: "12345",
+            phone: "54321",
+            userStatus: 1   
+         }
+         ).then(response => {
+            expect(response.status).to.eq(200)
+        })
+    })
+})
