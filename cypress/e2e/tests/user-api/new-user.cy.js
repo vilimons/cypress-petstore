95cb706aca95b738ff80c703259854166d7e1242
unknown <limavibrandao@gmail.com>
2023-08-18 17:49:46 -0300

    POST new user test successfully created returning code 200

--- cypress/e2e/tests/user-api/new-user.cy.js
+++ cypress/e2e/tests/user-api/new-user.cy.js
@@ -2,9 +2,19 @@
 
 describe('POST /user API', () => {
     it('should return 200 when a new user is successfully created', () => {
-        cy.request('GET', 'https://petstore.swagger.io/v2/pet/findByStatus?status=available').then(response => {
+        cy.request('POST', 
+                  'https://petstore.swagger.io/v2/user',
+                  {
+                    id: 1,
+                    username: "bob",
+                    firstname: "bob",
+                    lastname: "mcdonald",
+                    email: "bobmcdonaldo@email.com",
+                    password: '12345',
+                    phone: '54321',
+                    userStatus: 0
+                   }).then(response => {
             expect(response.status).to.eq(200)
-            expect(response.body).length.to.be.greaterThan(1)
         })
     })
 })
