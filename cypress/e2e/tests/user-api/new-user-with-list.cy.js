206f6593b93797cac2bc44c9a6c0c0321dab839e
unknown <limavibrandao@gmail.com>
2023-08-18 17:58:56 -0300

    POST new user with list test successfully created returning code 200

---
+++ cypress/e2e/tests/user-api/new-user-with-list.cy.js
@@ -0,0 +1,24 @@
+/// <reference types='Cypress' />
+
+const USER_OBJECT_LIST = [
+    {
+      id: 2,
+      username: "maria",
+      firstName: "maria",
+      lastName: "ramirez",
+      email: "mariaramirez@email.com",
+      password: '12345',
+      phone: '12345',
+      userStatus: 1,
+    },
+  ];
+
+describe('POST /user API', () => {
+    it('should return 200 when a new user is successfully created', () => {
+        cy.request('POST', 
+                  'https://petstore.swagger.io/v2/user/createWithList',
+                    USER_OBJECT_LIST).then(response => {
+            expect(response.status).to.eq(200)
+        })
+    })
+})
