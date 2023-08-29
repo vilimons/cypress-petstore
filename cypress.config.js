b3eed75768fcdcb300d8825e6177bd68e3b7ee4c
unknown <limavibrandao@gmail.com>
2023-08-29 16:30:41 -0300

    add new base url and changed the code to use this new base url

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://petstore.swagger.io/v2/',
     setupNodeEvents(on, config) {
       specPattern: 'cypress/integration/*.js'
     },
--- cypress/e2e/tests/pet-api/new-pet.cy.js
+++ cypress/e2e/tests/pet-api/new-pet.cy.js
@@ -3,7 +3,7 @@
 describe('POST /pet API', () => {
         
     it('should return 200 when a new pet is successfully created ', () => {
-        cy.request('POST', 'https://petstore.swagger.io/v2/pet', {
+        cy.request('POST', '/pet', {
                 id: 0,
                 "category": {
                   "id": 0,
